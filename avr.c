37d4d7f9be36143813211e350f3ff08b14d73f08
Ivan Tikhonov <kefeer@brokestream.com>
2010-10-19 19:00:28 +0600

    LDI executed

--- avr.c
+++ avr.c
@@ -18,6 +18,79 @@ void reset() {
 	pc=0;
 }
 
+void setreg(int r, uint8_t v) {
+	mem[r]=v;
+	printf("  r%u=%02x\n",r,v);
+}
+
+typedef int (*avr_inst)(uint16_t);
+
+#include "args.inc"
+
+int avr_LDI(uint16_t i) {
+        setreg(0x10+ARG_LDI_B,ARG_LDI_A);
+        return 1;
+}
+
+#define avr_UNIMPL (0)
+#define avr_STS avr_UNIMPL
+#define avr_LDS avr_UNIMPL
+#define avr_SBI avr_UNIMPL
+#define avr_CBI avr_UNIMPL
+#define avr_RCALL avr_UNIMPL
+#define avr_RJMP avr_UNIMPL
+#define avr_DEC avr_UNIMPL
+#define avr_MOVW avr_UNIMPL
+#define avr_OUT avr_UNIMPL
+#define avr_LPMZP avr_UNIMPL
+#define avr_LPMZ avr_UNIMPL
+#define avr_LDZP avr_UNIMPL
+#define avr_LDX avr_UNIMPL
+#define avr_LDDZ avr_UNIMPL
+#define avr_STXP avr_UNIMPL
+#define avr_STZP avr_UNIMPL
+#define avr_STX avr_UNIMPL
+#define avr_STY avr_UNIMPL
+#define avr_STZ avr_UNIMPL
+#define avr_STDY avr_UNIMPL
+#define avr_STDZ avr_UNIMPL
+#define avr_CPI avr_UNIMPL
+#define avr_ANDI avr_UNIMPL
+#define avr_ORI avr_UNIMPL
+#define avr_SBCI avr_UNIMPL
+#define avr_CPC avr_UNIMPL
+#define avr_ADC avr_UNIMPL
+#define avr_PUSH avr_UNIMPL
+#define avr_POP avr_UNIMPL
+#define avr_LSR avr_UNIMPL
+#define avr_ROR avr_UNIMPL
+#define avr_IN avr_UNIMPL
+#define avr_EOR avr_UNIMPL
+#define avr_AND avr_UNIMPL
+#define avr_ADD avr_UNIMPL
+#define avr_SUB avr_UNIMPL
+#define avr_SUBI avr_UNIMPL
+#define avr_STS16 avr_UNIMPL
+#define avr_SBIW avr_UNIMPL
+#define avr_MOV avr_UNIMPL
+#define avr_OR avr_UNIMPL
+#define avr_CP avr_UNIMPL
+#define avr_ADIW avr_UNIMPL
+#define avr_SBRS avr_UNIMPL
+#define avr_SBRC avr_UNIMPL
+#define avr_BRNE avr_UNIMPL
+#define avr_BRCS avr_UNIMPL
+#define avr_BREQ avr_UNIMPL
+#define avr_BRGE avr_UNIMPL
+#define avr_BRCC avr_UNIMPL
+#define avr_BRPL avr_UNIMPL
+#define avr_RET avr_UNIMPL
+#define avr_RETI avr_UNIMPL
+#define avr_SEC avr_UNIMPL
+#define avr_SEI avr_UNIMPL
+#define avr_CLI avr_UNIMPL
+#define avr_NOP avr_UNIMPL
+
 #include "decode.inc"
 
 void run() {
@@ -30,11 +103,21 @@ void run() {
 			sprintf(buf,"grep '^%4x' disasm.txt",pc*2);
 			system(buf);
 			return;
+
 		} else if(f<0) {
 			f=-f;
 			pc++;
 		}
 		printf("%s\n",inst_names[f]);
+		if(inst_funcs[f]) {
+			inst_funcs[f](i);
+		} else {
+			printf("instruction %s (%04x at %04x) not implemented\n",inst_names[f],i,pc*2);
+			char buf[1024];
+			sprintf(buf,"grep '^%4x' disasm.txt",pc*2);
+			system(buf);
+			return;
+		}
 		pc++;
 	}
 }
--- gendecode.py
+++ gendecode.py
@@ -1,9 +1,11 @@
 decode=['int decode(uint16_t i) {']
 enum=[]
+decoders=[]
 
 for x in open('inst.table'):
 	if x=='\n': continue
 	mask,opcode,name,pat=x.split()
+	decoders.append((name,pat))
 	if pat.startswith('D'): sign='-'
 	else: sign=''
 	decode.append("\tif((i&%s)==%s) return %sINST_%s;"%(mask,opcode,sign,name))
@@ -13,9 +15,72 @@ decode.append("\t return -1;\n}")
 
 decode.insert(0,('enum {INST_UNKNOWN,INST_')+(',INST_'.join(enum))+'};\n\n')
 decode.insert(1,('char *inst_names[]={"UNKNOWN","')+('","'.join(enum))+'"};\n\n')
+decode.insert(1,('avr_inst inst_funcs[]={avr_UNIMPL,avr_')+(',avr_'.join(enum))+'};\n\n')
 
-open('decode.inc','w').write('\n'.join(decode))
+def make_decoder(p):
+	v=p.split('_')
+	if 'D' in p: return
 
+	args=[]
+	pos=16
+	for x in v:
+		c=x[0]
+		if c=='i':
+			n=int(x[1:])
+		elif c=='a':
+			n=int(x[1:])
+			a=('u',pos-n,(1<<n)-1,n)
+			if len(args)==0: args.append([a])
+			else: args[0].append(a)
+		elif c=='b':
+			n=int(x[1:])
+			a=('u',pos-n,(1<<n)-1,n)
+			if len(args)==1: args.append([a])
+			else: args[1].append(a)
+		elif c=='s':
+			n=int(x[1:])
+			args.append([('s',pos-n,(1<<n)-1,n)])
+		else:
+			n=int(x)
+			args.append([('u',pos-n,(1<<n)-1,n)])
+		pos-=n
+	assert pos==0
+	return args
+
+def translate_decoder(pre,d):
+	args=[]
+	print d
+	for n,x in zip('AB',d):
+		if len(x)==2:
+			a,b=x
+			_,ash,am,aw=a
+			_,bsh,bm,bw=b
+			r="#define ARG_%s_%s ((((i>>%u)&0x%x)<<%u)|((i>>%u)&0x%x))"%(pre,n,ash,am,bw,bsh,bm)
+		elif len(x)==3:
+			a,b,c=x
+			_,ash,am,aw=a
+			_,bsh,bm,bw=b
+			_,csh,cm,cw=c
+			r="#define ARG_%s_%s ((((i>>%u)&0x%x)<<%u)|(((i>>%u)&0x%x)<<%u)|((i>>%u)&0x%x))"%(pre,n,ash,am,bw,bsh,bm,cw,csh,cm)
+		else:
+			sign,shift,mask,w=x[0]
+			r="#define ARG_%s_%s ((i>>%u)&0x%x)"%(pre,n,shift,mask)
+		args.append(r)
+	return args
+			
+	
+
+f=open('decode.inc','w')
+f.write('\n'.join(decode))
+f.close()
+
+f=open('args.inc','w')
+
+for n,p in decoders:
+	d=make_decoder(p)
+	if d:
+		s=translate_decoder(n,d)
+		f.write('\n'.join(s)+'\n')
 
 
 
